a6ae4ecccbada77910e1df470eada5158b8eefd3
Sabal Niroula <sabalniroula5555@gmail.com>
2020-12-03 19:51:24 +0545

    initial commit

---
+++ for_loops.java
@@ -0,0 +1,15 @@
+class test {
+
+public static void main(String [] args ){
+		
+		int len = 5;
+		String word = "sabalNiroula";
+		
+		for(int i =0;i<1;i++){
+		
+			System.out.println("fuck you bitch");	
+		}
+		
+	}
+
+}
